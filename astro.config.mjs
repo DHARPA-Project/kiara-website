23d1a6f8def41e893be17d8e1cf6ee1bb31679a4
Caro Appleby <caroline.appleby@cantab.net>
2024-02-15 10:39:02 +0000

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,50 +3,49 @@ import starlight from "@astrojs/starlight";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "kiara",
-      social: {
-        github: "https://github.com/DHARPA-project/kiara-website",
-      },
-      sidebar: [
-        {
-          label: "Concepts",
-          autogenerate: {
-            directory: "concepts",
-          },
-        },
-        {
-          label: "Researcher",
-          autogenerate: {
-            directory: "users",
-          },
-        },
-        {
-          label: "Developer",
-          collapsed: true,
-          autogenerate: {
-            directory: "developer",
-          },
-        },
-        {
-          label: "Internal",
-          collapsed: true,
-          autogenerate: {
-            directory: "internal",
-          },
-        },
-      ],
-    }),
-    react(),
-    markdoc({ allowHTML: true }),
-    keystatic(),
-  ],
+  integrations: [starlight({
+    title: "kiara",
+    social: {
+      github: "https://github.com/DHARPA-project/kiara-website"
+    },
+    sidebar: [{
+      label: "Concepts",
+      autogenerate: {
+        directory: "concepts"
+      }
+    }, {
+      label: "Researcher",
+      autogenerate: {
+        directory: "users"
+      }
+    }, {
+      label: "Developer",
+      collapsed: true,
+      autogenerate: {
+        directory: "developer"
+      }
+    }, {
+      label: "Internal",
+      collapsed: true,
+      autogenerate: {
+        directory: "internal"
+      }
+    }, {
+      label: "Plugins",
+      collapsed: true,
+      autogenerate: {
+        directory: "plugins"
+      }
+    }]
+  }), react(), markdoc({
+    allowHTML: true
+  }), keystatic(), svelte()],
   output: "hybrid",
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
